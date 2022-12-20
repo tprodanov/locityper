33282430793bd46c1bb0285503a96e8148a0dda9
Timofey Prodanov <timofey.prodanov@gmail.com>
2022-12-20 18:42:51 +0100

    Working on GeneralVec

--- src/algo/vec.rs
+++ src/algo/vec.rs
@@ -1,65 +1,73 @@
 use std::cmp::Ordering;
 use std::fmt::{Display, Debug};
-use std::ops::Index;
+use std::ops::{Index, IndexMut};
 
 use num_traits::Zero;
 use nalgebra::DVector;
 
 /// Reorders `storage` by taking elements in the order of `ixs`.
-/// Returns reordered iterator.
-pub fn reorder<T, V, I>(storage: V, ixs: I) -> Reorder<T, V, I>
-where T: Copy,
-      V: Index<usize, Output = T>,
-      I: Iterator<Item = usize>
-{
-    Reorder { storage, ixs }
-}
-
-pub struct Reorder<T, V, I>
-where T: Copy,
-      V: Index<usize, Output = T>,
-      I: Iterator<Item = usize>
-{
-    storage: V,
-    ixs: I,
-}
-
-impl<T, V, I> Iterator for Reorder<T, V, I>
-where T: Copy,
-      V: Index<usize, Output = T>,
-      I: Iterator<Item = usize>
-{
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.ixs.next() {
-            Some(ix) => Some(*self.storage.index(ix)),
-            None => None,
-        }
-    }
-
-    #[inline]
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.ixs.size_hint()
-    }
-}
-
-impl<T, V, I> ExactSizeIterator for Reorder<T, V, I>
+/// Returns `Vec`.
+pub fn reorder_vec<T, V, I>(storage: V, ixs: I) -> Vec<T>
 where T: Copy,
       V: Index<usize, Output = T>,
       I: Iterator<Item = usize> + ExactSizeIterator
 {
-    #[inline]
-    fn len(&self) -> usize {
-        self.ixs.len()
+    let n = ixs.len();
+    let mut res = Vec::with_capacity(n);
+    for i in ixs {
+        res.push(storage[i]);
+    }
+    res
+}
+
+/// Reorders `storage` by taking elements in the order of `ixs`.
+/// Returns `DVector`.
+pub fn reorder_dvec<T, V, I>(storage: V, ixs: I) -> DVector<T>
+where T: Copy + Debug + Eq + Zero + 'static,
+      V: Index<usize, Output = T>,
+      I: Iterator<Item = usize> + ExactSizeIterator
+{
+    let n = ixs.len();
+    let mut res = DVector::zeros(n);
+    for (i, j) in ixs.enumerate() {
+        res[i] = storage[j]
+    }
+    res
+}
+
+pub trait Scalar : nalgebra::Scalar + Zero + Copy { }
+
+pub trait GeneralVec<T: Scalar> : Index<usize, Output = T> + IndexMut<usize, Output = T> {
+    fn create_vec(n: usize) -> Self;
+}
+
+impl<T: Scalar> GeneralVec<T> for Vec<T> {
+    fn create_vec(n: usize) -> Self {
+        vec![T::zero(); n]
     }
 }
 
-impl<T, V, I> std::iter::FusedIterator for Reorder<T, V, I>
-where T: Copy,
+impl<T: Scalar> GeneralVec<T> for DVector<T> {
+    fn create_vec(n: usize) -> Self {
+        DVector::zeros(n)
+    }
+}
+
+/// Reorders `storage` by taking elements in the order of `ixs`.
+/// Returns `DVector`.
+pub fn reorder<T, V, I, W>(storage: V, ixs: I) -> W
+where T: Scalar,
       V: Index<usize, Output = T>,
-      I: Iterator<Item = usize> + std::iter::FusedIterator
-{ }
+      I: Iterator<Item = usize> + ExactSizeIterator,
+      W: GeneralVec<T>
+{
+    let n = ixs.len();
+    let mut res = W::create_vec(n);
+    for (i, j) in ixs.enumerate() {
+        res[i] = storage[j]
+    }
+    res
+}
 
 /// Performs binary search and finds the index `i` such that `v[i-1] < target <= v[i]`.
 pub fn binary_search_left<T: PartialOrd + Display>(v: &[T], target: T) -> usize {
@@ -78,21 +86,21 @@ pub fn binary_search_left<T: PartialOrd + Display>(v: &[T], target: T) -> usize 
     low
 }
 
-/// Performs binary search and finds the index `i` such that `v[i-1] <= target < v[i]`.
-pub fn binary_search_right(v: &[f64], target: f64) -> usize {
-    let n = v.len();
-    let mut low = 0;
-    let mut high = n;
-    while low < high {
-        let mid = (low + high) / 2;
-        if v[mid] > target {
-            if mid == 0 || v[mid - 1] == target {
-                return mid;
-            }
-            high = mid;
-        } else {
-            low = mid + 1;
-        }
-    }
-    low
-}
+// /// Performs binary search and finds the index `i` such that `v[i-1] <= target < v[i]`.
+// pub fn binary_search_right<T: PartialOrd>(v: &[T], target: T) -> usize {
+//     let n = v.len();
+//     let mut low = 0;
+//     let mut high = n;
+//     while low < high {
+//         let mid = (low + high) / 2;
+//         if v[mid] > target {
+//             if mid == 0 || v[mid - 1] == target {
+//                 return mid;
+//             }
+//             high = mid;
+//         } else {
+//             low = mid + 1;
+//         }
+//     }
+//     low
+// }
