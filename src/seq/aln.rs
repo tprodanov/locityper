9b21060acc07aa22562f5799623f15453c06abc9
Timofey Prodanov <timofey.prodanov@gmail.com>
2022-12-26 21:57:40 +0100

    Parse MD tag

--- src/seq/aln.rs
+++ src/seq/aln.rs
@@ -1,117 +1,418 @@
 //! Various functions and structures related to sequence alignment.
 
-use std::fmt::Write;
+use std::fmt::{self, Write};
+use std::cmp::min;
 
-use lazy_static::lazy_static;
-use regex::Regex;
-use rust_htslib::bam::record::Cigar;
+use rust_htslib::bam::record;
 
-/// Does the Cigar operation consume reference sequence?
-const fn consumes_ref(op: Cigar) -> bool {
-    match op {
-        Cigar::Match(_) | Cigar::Del(_) | Cigar::RefSkip(_) | Cigar::Equal(_) | Cigar::Diff(_) => true,
-        _ => false,
+/// Subset of CIGAR operations.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Operation {
+    Match,
+    Equal,
+    Diff,
+    Soft,
+    Ins,
+    Del,
+}
+
+impl Operation {
+    /// Does the Cigar operation consume reference sequence?
+    pub const fn consumes_ref(self) -> bool {
+        match self {
+            Operation::Match | Operation::Equal | Operation::Diff | Operation::Del => true,
+            _ => false,
+        }
+    }
+
+    /// Does the Cigar operation consume query sequence?
+    pub const fn consumes_query(self) -> bool {
+        match self {
+            Operation::Match | Operation::Equal | Operation::Diff | Operation::Ins | Operation::Soft => true,
+            _ => false,
+        }
+    }
+
+    /// Does the Cigar operation consume both reference and query sequences?
+    pub const fn consumes_both(self) -> bool {
+        match self {
+            Operation::Match | Operation::Equal | Operation::Diff => true,
+            _ => false,
+        }
+    }
+
+    /// Convert operation into char.
+    pub const fn to_char(self) -> char {
+        match self {
+            Operation::Match => 'M',
+            Operation::Equal => '=',
+            Operation::Diff => 'X',
+            Operation::Ins => 'I',
+            Operation::Del => 'D',
+            Operation::Soft => 'S',
+        }
+    }
+
+    /// Get operation from a raw u32 value.
+    pub const fn from_u32(val: u32) -> Operation {
+        match val {
+            0 => Operation::Match,
+            1 => Operation::Ins,
+            2 => Operation::Del,
+            // 3 => RefSkip,
+            4 => Operation::Soft,
+            // 5 => Hard,
+            // 6 => Padding,
+            7 => Operation::Equal,
+            8 => Operation::Diff,
+            _ => panic!("Unexpected cigar operation"),
+        }
     }
 }
 
-/// Does the Cigar operation consume query sequence?
-const fn consumes_query(op: Cigar) -> bool {
-    match op {
-        Cigar::Match(_) | Cigar::Ins(_) | Cigar::SoftClip(_) | Cigar::Equal(_) | Cigar::Diff(_) => true,
-        _ => false,
+impl fmt::Display for Operation {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_char(self.to_char())
     }
 }
 
-/// Does the Cigar operation consume both reference and query sequences?
-const fn consumes_both(op: Cigar) -> bool {
-    match op {
-        Cigar::Match(_) | Cigar::Equal(_) | Cigar::Diff(_) => true,
-        _ => false,
+/// Tuple operation + length.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct CigarItem {
+    op: Operation,
+    len: u32,
+}
+
+impl CigarItem {
+    /// Creates a new tuple.
+    pub fn new(op: Operation, len: u32) -> Self {
+        CigarItem { op, len }
+    }
+
+    /// Get operation.
+    pub fn operation(&self) -> Operation {
+        self.op
+    }
+
+    /// Get length.
+    pub fn len(&self) -> u32 {
+        self.len
+    }
+
+    /// Creates a new tuple from a raw u32 value.
+    pub fn from_u32(val: u32) -> Self {
+        Self {
+            op: Operation::from_u32(val & 0b1111),
+            len: val >> 4,
+        }
+    }
+
+    /// Creates a new tuple from HTSLIB format.
+    pub fn from_htslib(op: record::Cigar) -> Self {
+        match op {
+            record::Cigar::Match(len) => CigarItem::new(Operation::Match, len),
+            record::Cigar::Ins(len) => CigarItem::new(Operation::Ins, len),
+            record::Cigar::Del(len) => CigarItem::new(Operation::Del, len),
+            record::Cigar::SoftClip(len) => CigarItem::new(Operation::Soft, len),
+            record::Cigar::Equal(len) => CigarItem::new(Operation::Equal, len),
+            record::Cigar::Diff(len) => CigarItem::new(Operation::Diff, len),
+            _ => panic!("Unexpected CIGAR operation {}", op.char()),
+        }
     }
 }
 
-pub trait StrOrNone {
+impl fmt::Display for CigarItem {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}{}", self.len, self.op)
+    }
+}
+
+/// Wrapper over vector of `CigarItem`.
+#[derive(Default, Debug, Clone)]
+pub struct Cigar {
+    tuples: Vec<CigarItem>,
+    /// Reference length.
+    rlen: u32,
+    /// Query length.
+    qlen: u32,
+}
+
+impl Cigar {
+    /// Create an empty CIGAR.
+    #[inline]
+    pub fn new() -> Cigar {
+        Cigar::default()
+    }
+
+    /// Length of the reference sequence.
+    pub fn ref_len(&self) -> u32 {
+        self.rlen
+    }
+
+    /// Length of the query sequence.
+    pub fn query_len(&self) -> u32 {
+        self.qlen
+    }
+
+    /// Push a new entry, does not merge with the latest entry.
+    pub fn unchecked_push(&mut self, op: Operation, len: u32) {
+        if op.consumes_ref() {
+            self.rlen += len;
+        }
+        if op.consumes_query() {
+            self.qlen += len;
+        }
+        self.tuples.push(CigarItem::new(op, len))
+    }
+
+    /// Push a new entry, merges with the latest entry, if relevant.
+    pub fn push(&mut self, op: Operation, len: u32) {
+        let n = self.tuples.len();
+        if n == 0 || self.tuples[n - 1].op != op {
+            self.unchecked_push(op, len);
+        } else {
+            if op.consumes_ref() {
+                self.rlen += len;
+            }
+            if op.consumes_query() {
+                self.qlen += len;
+            }
+            self.tuples[n - 1].len += len;
+        }
+    }
+
+    /// Returns iterator over CIGAR items.
+    pub fn iter<'a>(&'a self) -> std::slice::Iter<'a, CigarItem> {
+        self.tuples.iter()
+    }
+}
+
+impl PartialEq for Cigar {
+    fn eq(&self, oth: &Self) -> bool {
+        self.tuples.eq(&oth.tuples)
+    }
+}
+
+impl Eq for Cigar {}
+
+impl fmt::Display for Cigar {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for tup in self.iter() {
+            write!(f, "{}", tup)?
+        }
+        Ok(())
+    }
+}
+
+/// Trait that stores either Vec<u8>, or ().
+pub trait SeqOrNone {
+    /// Creates a new object.
     fn new() -> Self;
 
-    fn append(&mut self, ch: char);
+    /// Pushes a new nucleotide.
+    fn push(&mut self, nt: u8);
 
-    fn extend(&mut self, s: &str);
+    /// Complete the object creation (for example, shrinks the vector).
+    fn complete(&mut self);
+
+    /// Returns length, if available.
+    fn len(&self) -> Option<usize>;
 }
 
-impl StrOrNone for () {
-    fn new() -> Self { }
-
-    fn append(&mut self, _: char) { }
-
-    fn extend(&mut self, _: &str) { }
+impl SeqOrNone for () {
+    fn new() -> Self {}
+    fn push(&mut self, _nt: u8) {}
+    fn complete(&mut self) {}
+    fn len(&self) -> Option<usize> { None }
 }
 
-impl StrOrNone for String {
+impl SeqOrNone for Vec<u8> {
     fn new() -> Self {
-        String::new()
+        Vec::new()
     }
 
-    fn append(&mut self, ch: char) {
-        self.write_char(ch).unwrap()
+    fn push(&mut self, nt: u8) {
+        self.push(nt);
     }
 
-    fn extend(&mut self, s: &str) {
-        self.write_str(s).unwrap()
+    fn complete(&mut self) {
+        self.shrink_to_fit();
+    }
+
+    fn len(&self) -> Option<usize> {
+        Some(self.len())
     }
 }
 
-enum MdEntry<'a> {
-    Match(u16),
-    Mismatch(&'a str),
-    Deletion(&'a str),
+/// Entry in a MD tag.
+#[derive(Clone, Debug)]
+enum MdEntry {
+    /// Match(length): reference matches with the query for `length` nucleotides,
+    Match(u32),
+    /// Mismatch(start, end): there is a mismatch, see reference sequence in md_tag[start..end],
+    Mismatch(u32, u32),
+    /// Deletion(start, end): there is a deletion, see reference sequence in md_tag[start..end].
+    Deletion(u32, u32),
 }
 
-fn parse_md<'a>(md: &'a str) -> Vec<MdEntry<'a>> {
-    lazy_static! {
-        static ref RE_NUMBER: Regex = Regex::new("[0-9]+").unwrap();
-    }
-    let md_bytes = md.as_bytes();
-    let mut res = Vec::new();
-    let mut last_end = 0;
-    for m in RE_NUMBER.find_iter(md) {
-        if last_end > 0 {
-            let dist = last_end - m.start();
-            assert!(dist > 0, "Failed to parse MD tag {}", md);
-            if md_bytes[last_end] == b'^' {
-                assert!(dist > 1, "Failed to parse MD tag {}", md);
-                res.push(MdEntry::Deletion(&md[last_end + 1..m.start()]));
+/// Parse MD tag, returns Vector of `MdEntry`.
+fn parse_md(md: &[u8]) -> Vec<MdEntry> {
+    const MATCH: u8 = 0;
+    const MISM: u8 = 1;
+    const DEL: u8 = 2;
+    let mut status = MATCH;
+    // Value is used as length for match, start index for mismatch and deletion.
+    let mut value: u32 = 0;
+    let mut md_entries = Vec::new();
+    for (i, &byte) in md.iter().enumerate() {
+        let i = i as u32;
+        if b'0' <= byte && byte <= b'9' {
+            if status >= MISM {
+                debug_assert!(value < i);
+                md_entries.push(
+                    if status == MISM { MdEntry::Mismatch(value, i) } else { MdEntry::Deletion(value, i) });
+                status = MATCH;
+                value = (byte - b'0') as u32;
             } else {
-                res.push(MdEntry::Mismatch(&md[last_end..m.start()]));
+                value = 10 * value + (byte - b'0') as u32;
             }
-        } else {
-            assert_eq!(m.start(), 0, "Failed to parse MD tag {}", md);
         }
-        let length = m.as_str().parse().unwrap();
-        if length > 0 {
-            res.push(MdEntry::Match(length));
+
+        else if byte == b'^' {
+            debug_assert!(status == MATCH);
+            if value > 0 {
+                md_entries.push(MdEntry::Match(value));
+            }
+            status = DEL;
+            value = i + 1;
         }
-        last_end = m.end();
+
+        else if status == MATCH {
+            if value > 0 {
+                md_entries.push(MdEntry::Match(value));
+            }
+            status = MISM;
+            value = i;
+        }
     }
-    assert_eq!(last_end, md.len(), "Failed to parse MD tag {}", md);
-    res
+    debug_assert!(status == MATCH);
+    if value > 0 {
+        md_entries.push(MdEntry::Match(value));
+    }
+    md_entries
 }
 
-pub fn to_extended_cigar<Out: StrOrNone>(cigar: impl Iterator<Item = Cigar>, md: &str)
-        -> (Vec<Operation>, Out) {
-    let mut qpos = 0;
-    let mut rpos = 0;
-    let mut new_cigar = Vec::new();
-    let mut ref_seq = Out::new();
+/// Temporary data structure, used to reduce the number of function parameters.
+struct ExtCigarData<'a, S: SeqOrNone> {
+    query_seq: record::Seq<'a>,
+    new_cigar: Cigar,
+    ref_seq: S,
 
-    let md_entries = parse_md(md);
-    let mut md_ix = 0;
-    let mut md_shift = 0;
+    md_str: &'a str,
+    raw_md: &'a [u8],
+    md_entries: Vec<MdEntry>,
+    md_ix: usize,
+    md_shift: u32,
+}
 
-    for op in cigar {
-        if consumes_both(op) {
-            
+impl<'a, S: SeqOrNone> ExtCigarData<'a, S> {
+    fn process(query_seq: record::Seq<'a>, raw_cigar: &[u32], md_str: &'a str) -> (Cigar, S) {
+        let raw_md = md_str.as_bytes();
+        let mut data = Self {
+            query_seq,
+            new_cigar: Cigar::new(),
+            ref_seq: S::new(),
+            md_str,
+            raw_md,
+            md_entries: parse_md(raw_md),
+            md_ix: 0,
+            md_shift: 0,
+        };
+
+        for &val in raw_cigar {
+            data.process_op(CigarItem::from_u32(val));
         }
+
+        if let Some(ref_len) = data.ref_seq.len() {
+            assert_eq!(ref_len as u32, data.new_cigar.ref_len(), "Failed to parse MD tag \"{}\"", md_str);
+        }
+        assert_eq!(data.new_cigar.query_len(), query_seq.len() as u32, "Failed to parse MD tag \"{}\"", md_str);
+
+        (data.new_cigar, data.ref_seq)
     }
 
-    (new_cigar, ref_seq)
+    fn process_op(&mut self, tup: CigarItem) {
+        if tup.op.consumes_both() {
+            let mut op_len = tup.len;
+            while op_len != 0 {
+                match &self.md_entries[self.md_ix] {
+                    MdEntry::Match(length) => self.process_match_match(&mut op_len, *length),
+                    MdEntry::Mismatch(start, end) => self.process_match_mismatch(&mut op_len, *start, *end),
+                    MdEntry::Deletion(_, _) => unreachable!(
+                        "Failed to parse MD tag \"{}\": operation M coincides with deletion", self.md_str),
+                }
+            }
+
+        } else if tup.op.consumes_query() {
+            self.new_cigar.unchecked_push(tup.op, tup.len);
+
+        } else {
+            if let MdEntry::Deletion(start, end) = self.md_entries[self.md_ix] {
+                debug_assert_eq!(self.md_shift, 0);
+                debug_assert_eq!(end - start, tup.len);
+                for i in start..end {
+                    self.ref_seq.push(self.raw_md[i as usize]);
+                }
+                self.new_cigar.unchecked_push(tup.op, tup.len);
+                self.md_ix += 1;
+            }
+        }
+        debug_assert_eq!(self.md_ix, self.md_entries.len(), "Failed to parse MD tag \"{}\"", self.md_str);
+    }
+
+    fn process_match_match(&mut self, op_len: &mut u32, md_len: u32) {
+        let rem_md_len = md_len - self.md_shift;
+        let pos_inc = min(*op_len, rem_md_len);
+        for qpos in self.new_cigar.rlen..self.new_cigar.rlen + pos_inc {
+            self.ref_seq.push(self.query_seq[qpos as usize]);
+        }
+
+        if rem_md_len > pos_inc {
+            self.md_shift += pos_inc;
+        } else {
+            self.md_ix += 1;
+            self.md_shift = 0;
+        }
+        self.new_cigar.push(Operation::Equal, pos_inc);
+        *op_len -= pos_inc;
+    }
+
+    fn process_match_mismatch(&mut self, op_len: &mut u32, md_start: u32, md_end: u32) {
+        let rem_md_len = md_end - md_start - self.md_shift;
+        let pos_inc = min(*op_len, rem_md_len);
+        for pos in md_start + self.md_shift..md_start + self.md_shift + pos_inc {
+            self.ref_seq.push(self.raw_md[pos as usize]);
+        }
+
+        if rem_md_len > pos_inc {
+            self.md_shift += pos_inc;
+        } else {
+            self.md_ix += 1;
+            self.md_shift = 0;
+        }
+        self.new_cigar.push(Operation::Diff, pos_inc);
+        *op_len -= pos_inc;
+    }
+}
+
+/// Create an extended CIGAR from short CIGAR and MD string.
+/// Returns pair (Cigar, S), where S is either `Vec<u8>` and `()`.
+pub fn infer_extended_cigar<S: SeqOrNone>(rec: &record::Record) -> (Cigar, S) {
+    let md_str = if let Ok(record::Aux::String(s)) = rec.aux(b"MD") {
+        s
+    } else {
+        panic!("Cannot create extended CIGAR: record {} either has no MD tag, or MD tag has incorrect type",
+            String::from_utf8_lossy(rec.qname()))
+    };
+    ExtCigarData::process(rec.seq(), rec.raw_cigar(), md_str)
 }
