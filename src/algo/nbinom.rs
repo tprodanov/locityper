b1c369e90a5058c6d5ab32c7869dbee0cca54d9d
Timofey Prodanov <timofey.prodanov@gmail.com>
2023-01-09 17:07:14 +0100

    Improve NBinom

--- src/algo/nbinom.rs
+++ src/algo/nbinom.rs
@@ -1,19 +1,14 @@
 use std::fmt::{self, Debug, Display, Formatter};
-use std::cell::Cell;
+use once_cell::unsync::OnceCell;
 use statrs::{
     statistics::{Min, Max},
-    distribution::{Discrete, DiscreteCDF},
+    distribution::Discrete,
     function::{
         beta::beta_reg,
         gamma::ln_gamma,
     },
 };
 use crate::bg::ser::{JsonSer, LoadError};
-
-use std::time::{Duration, Instant};
-use statrs::distribution::NegativeBinomial;
-use rand::{thread_rng, Rng};
-use rand::prelude::Distribution;
 
 /// Negative Binomial distribution,
 /// similar to `statrs::distribution::NegativeBinomial`, but storing some precalculated values.
@@ -56,13 +51,13 @@ impl NBinom {
 
     /// Creates a cached Negative Binomial distribution, storing ln_pmf values from 0 up to <0.999 quantile>.
     pub fn cached_q999(self) -> CachedDistr<Self> {
-        let q999: u64 = self.inverse_cdf(0.999);
-        CachedDistr::new(self, q999 as usize)
+        let q999 = self.quantile(0.999).ceil() as usize;
+        CachedDistr::new(self, q999 + 1)
     }
 
     /// Createts a cached Negative Binomial distribution, storing ln_pmf values in 0..=n.
-    pub fn cached(self, n: usize) -> CachedDistr<Self> {
-        CachedDistr::new(self, n)
+    pub fn cached(self, cache_size: usize) -> CachedDistr<Self> {
+        CachedDistr::new(self, cache_size)
     }
 
     pub fn n(&self) -> f64 {
@@ -81,8 +76,52 @@ impl NBinom {
         self.n * (1.0 - self.p) / (self.p * self.p)
     }
 
-    pub fn ln_pmf(&self, x: f64) -> f64 {
+    pub fn ln_pmf_f64(&self, x: f64) -> f64 {
         self.lnpmf_const + ln_gamma(self.n + x) - ln_gamma(x + 1.0) + x * self.lnq
+    }
+
+    pub fn cdf(&self, x: f64) -> f64 {
+        beta_reg(self.n, x + 1.0, self.p)
+    }
+
+    pub fn sf(&self, x: f64) -> f64 {
+        beta_reg(x + 1.0, self.n, 1.0 - self.p)
+    }
+
+    pub fn quantile(&self, q: f64) -> f64 {
+        if q <= 0.0 {
+            return 0.0;
+        }
+        if q >= 1.0 {
+            return f64::INFINITY;
+        }
+        let mut low = 0;
+        let mut high = (2.0 * self.mean()) as u64;
+        while self.cdf(high as f64) < q {
+            low = high;
+            high *= 2;
+        }
+        while high >= low {
+            let mid = (low + high) / 2;
+            if self.cdf(mid as f64) >= q {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        let k = high as f64;
+        let cdf0 = self.cdf(k);
+        assert!(cdf0 <= q);
+        let cdf1 = self.cdf(k + 1.0);
+        let diff = cdf1 - cdf0;
+        match diff.total_cmp(&0.0) {
+            std::cmp::Ordering::Equal => k,
+            std::cmp::Ordering::Greater => {
+                let r = (q - cdf0) / diff;
+                k * (1.0 - r) + (k + 1.0) * r
+            }
+            _ => panic!("Assertion failed: CDF is non-increasing!"),
+        }
     }
 }
 
@@ -115,63 +154,42 @@ impl JsonSer for NBinom {
     }
 }
 
-macro_rules! nbinom_impl {
-    (
-        $int:ident
-    ) => {
-        impl Min<$int> for NBinom {
-            fn min(&self) -> $int {
-                0
-            }
-        }
-
-        impl Max<$int> for NBinom {
-            fn max(&self) -> $int {
-                std::$int::MAX
-            }
-        }
-
-        impl Discrete<$int, f64> for NBinom {
-            fn pmf(&self, x: $int) -> f64 {
-                self.ln_pmf(x as f64).exp()
-            }
-
-            fn ln_pmf(&self, x: $int) -> f64 {
-                self.ln_pmf(x as f64)
-            }
-        }
-
-        impl DiscreteCDF<$int, f64> for NBinom {
-            fn cdf(&self, x: $int) -> f64 {
-                panic!("Incorrect formula!");
-                beta_reg(self.n, (x + 1) as f64, self.p)
-            }
-
-            fn sf(&self, x: $int) -> f64 {
-                panic!("Incorrect formula!");
-                beta_reg((x + 1) as f64, self.n, 1.0 - self.p)
-            }
-        }
+impl Min<u64> for NBinom {
+    fn min(&self) -> u64 {
+        0
     }
 }
 
-nbinom_impl!(u32);
-nbinom_impl!(u64);
+impl Max<u64> for NBinom {
+    fn max(&self) -> u64 {
+        std::u64::MAX
+    }
+}
+
+impl Discrete<u64, f64> for NBinom {
+    fn pmf(&self, x: u64) -> f64 {
+        self.ln_pmf_f64(x as f64).exp()
+    }
+
+    fn ln_pmf(&self, x: u64) -> f64 {
+        self.ln_pmf_f64(x as f64)
+    }
+}
 
 /// Distribution with cached ln_pmf values.
 #[derive(Clone)]
 pub struct CachedDistr<D: Discrete<u64, f64>> {
     distr: D,
-    cache: Vec<Cell<f64>>,
+    cache: Vec<OnceCell<f64>>,
 }
 
 impl<D: Discrete<u64, f64>> CachedDistr<D> {
     /// Creates the cached distribution.
-    /// Stores ln_pmf values in 0..=n.
-    pub fn new(distr: D, n: usize) -> Self {
+    /// Caches ln_pmf values in `0..cache_size`.
+    pub fn new(distr: D, cache_size: usize) -> Self {
         CachedDistr {
             distr,
-            cache: vec![Cell::new(f64::NAN); n + 1],
+            cache: vec![OnceCell::new(); cache_size],
         }
     }
 
@@ -180,74 +198,7 @@ impl<D: Discrete<u64, f64>> CachedDistr<D> {
         &self.distr
     }
 
-    pub fn ln_pmf(&self, k: u64) -> f64 {
-        let i = k as usize;
-        if i < self.cache.len() {
-            let val = self.cache[i].get();
-            if !val.is_nan() {
-                return val;
-            }
-
-            let val = self.distr.ln_pmf(k);
-            self.cache[i].replace(val);
-            val
-        } else {
-            self.distr.ln_pmf(k)
-        }
-    }
-}
-
-impl<D: Discrete<u64, f64> + Debug> Debug for CachedDistr<D> {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "Cached[{:?}, 0..{}]", self.distr, self.cache.len())
-    }
-}
-
-impl<D: Discrete<u64, f64> + Display> Display for CachedDistr<D> {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "Cached[{}, 0..{}]", self.distr, self.cache.len())
-    }
-}
-
-impl<D: Discrete<u64, f64> + JsonSer> JsonSer for CachedDistr<D> {
-    fn save(&self) -> json::JsonValue {
-        json::object!{
-            distr: self.distr.save(),
-            count: self.cache.len(),
-        }
-    }
-
-    fn load(obj: &json::JsonValue) -> Result<Self, LoadError> {
-        if !obj.has_key("distr") {
-            return Err(LoadError(format!("CachedDistr: Failed to parse '{}': missing 'distr' field!", obj)));
-        }
-        let distr = D::load(&obj["distr"])?;
-        let count = obj["p"].as_usize().ok_or_else(|| LoadError(format!(
-            "CachedDistr: Failed to parse '{}': missing or incorrect 'count' field!", obj)))?;
-        Ok(Self::new(distr, count))
-    }
-}
-
-/// Distribution with cached ln_pmf values.
-#[derive(Clone)]
-pub struct CachedDistr2<D: Discrete<u64, f64>> {
-    distr: D,
-    cache: Vec<once_cell::unsync::OnceCell<f64>>,
-}
-
-impl<D: Discrete<u64, f64>> CachedDistr2<D> {
-    pub fn new(distr: D, n: usize) -> Self {
-        Self {
-            distr,
-            cache: vec![once_cell::unsync::OnceCell::new(); n + 1],
-        }
-    }
-
-    /// Get inner distrbution.
-    pub fn distr(&self) -> &D {
-        &self.distr
-    }
-
+    /// Returns ln(pmf(k)). Caches values in a certain range.
     pub fn ln_pmf(&self, k: u64) -> f64 {
         let i = k as usize;
         if i < self.cache.len() {
@@ -258,168 +209,33 @@ impl<D: Discrete<u64, f64>> CachedDistr2<D> {
     }
 }
 
-#[derive(Clone)]
-pub struct CachedDistr3<D: Discrete<u64, f64>> {
-    distr: D,
-    cache: std::cell::RefCell<intmap::IntMap<f64>>,
+impl<D: Discrete<u64, f64> + Debug> Debug for CachedDistr<D> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Cached[{:?}, size={}]", self.distr, self.cache.len())
+    }
 }
 
-impl<D: Discrete<u64, f64>> CachedDistr3<D> {
-    pub fn new(distr: D) -> Self {
-        Self {
-            distr,
-            cache: std::cell::RefCell::new(intmap::IntMap::new()),
+impl<D: Discrete<u64, f64> + Display> Display for CachedDistr<D> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Cached[{}, size={}]", self.distr, self.cache.len())
+    }
+}
+
+impl<D: Discrete<u64, f64> + JsonSer> JsonSer for CachedDistr<D> {
+    fn save(&self) -> json::JsonValue {
+        json::object!{
+            distr: self.distr.save(),
+            size: self.cache.len(),
         }
     }
 
-    /// Get inner distrbution.
-    pub fn distr(&self) -> &D {
-        &self.distr
-    }
-
-    pub fn ln_pmf(&self, k: u64) -> f64 {
-        match self.cache.borrow_mut().entry(k) {
-            intmap::Entry::Occupied(entry) => *entry.get(),
-            intmap::Entry::Vacant(entry) => {
-                let val = self.distr.ln_pmf(k);
-                entry.insert(val);
-                val
-            }
+    fn load(obj: &json::JsonValue) -> Result<Self, LoadError> {
+        if !obj.has_key("distr") {
+            return Err(LoadError(format!("CachedDistr: Failed to parse '{}': missing 'distr' field!", obj)));
         }
+        let distr = D::load(&obj["distr"])?;
+        let size = obj["size"].as_usize().ok_or_else(|| LoadError(format!(
+            "CachedDistr: Failed to parse '{}': missing or incorrect 'size' field!", obj)))?;
+        Ok(Self::new(distr, size))
     }
-}
-
-pub struct CachedDistr4<D: Discrete<u64, f64>> {
-    distr: D,
-    cache: std::cell::UnsafeCell<intmap::IntMap<f64>>,
-    in_use: std::cell::Cell<bool>,
-}
-
-impl<D: Discrete<u64, f64>> CachedDistr4<D> {
-    pub fn new(distr: D) -> Self {
-        Self {
-            distr,
-            cache: std::cell::UnsafeCell::new(intmap::IntMap::new()),
-            in_use: std::cell::Cell::new(false),
-        }
-    }
-
-    /// Get inner distrbution.
-    pub fn distr(&self) -> &D {
-        &self.distr
-    }
-
-    pub fn ln_pmf(&self, k: u64) -> f64 {
-        assert!(!self.in_use.get());
-        self.in_use.set(true);
-        let val;
-        unsafe {
-            let cache = self.cache.get();
-            // &*(*map).entry(k).or_insert(v)
-            match (*cache).entry(k) {
-                intmap::Entry::Occupied(entry) => val = *entry.get(),
-                intmap::Entry::Vacant(entry) => {
-                    val = self.distr.ln_pmf(k);
-                    entry.insert(val);
-                },
-            };
-        };
-        self.in_use.set(false);
-        val
-    }
-}
-
-#[derive(Clone)]
-pub struct CachedDistr5<D: Discrete<u64, f64>> {
-    distr: D,
-    cache: std::cell::RefCell<std::collections::HashMap<u64, f64>>,
-}
-
-impl<D: Discrete<u64, f64>> CachedDistr5<D> {
-    pub fn new(distr: D) -> Self {
-        Self {
-            distr,
-            cache: std::cell::RefCell::new(std::collections::HashMap::new()),
-        }
-    }
-
-    /// Get inner distrbution.
-    pub fn distr(&self) -> &D {
-        &self.distr
-    }
-
-    pub fn ln_pmf(&self, k: u64) -> f64 {
-        *self.cache.borrow_mut().entry(k).or_insert_with(|| self.distr.ln_pmf(k))
-    }
-}
-
-fn bench0(distr: &NBinom, s: usize, ks: &[u64]) {
-    let start = Instant::now();
-    let sum = ks.iter().map(|&k| distr.ln_pmf(k as f64)).sum::<f64>();
-    let duration = start.elapsed();
-    println!("NBinom:       {:?}   (sum {:>})", duration, sum);
-}
-
-fn bench1(distr: &NBinom, s: usize, ks: &[u64]) {
-    let start = Instant::now();
-    let distr1 = CachedDistr::new(distr.clone(), s);
-    let sum = ks.iter().map(|&k| distr1.ln_pmf(k)).sum::<f64>();
-    let duration = start.elapsed();
-    println!("CachedDistr1: {:?}   (sum {:>})", duration, sum);
-}
-
-fn bench2(distr: &NBinom, s: usize, ks: &[u64]) {
-    let start = Instant::now();
-    let distr1 = CachedDistr::new(distr.clone(), s);
-    let sum = ks.iter().map(|&k| distr1.ln_pmf(k)).sum::<f64>();
-    let duration = start.elapsed();
-    println!("CachedDistr2: {:?}   (sum {:>})", duration, sum);
-}
-
-fn bench3(distr: &NBinom, s: usize, ks: &[u64]) {
-    let start = Instant::now();
-    let distr1 = CachedDistr3::new(distr.clone());
-    let sum = ks.iter().map(|&k| distr1.ln_pmf(k)).sum::<f64>();
-    let duration = start.elapsed();
-    println!("CachedDistr3: {:?}   (sum {:>})", duration, sum);
-}
-
-fn bench4(distr: &NBinom, s: usize, ks: &[u64]) {
-    let start = Instant::now();
-    let distr1 = CachedDistr4::new(distr.clone());
-    let sum = ks.iter().map(|&k| distr1.ln_pmf(k)).sum::<f64>();
-    let duration = start.elapsed();
-    println!("CachedDistr4: {:?}   (sum {:>})", duration, sum);
-}
-
-fn bench5(distr: &NBinom, s: usize, ks: &[u64]) {
-    let start = Instant::now();
-    let distr1 = CachedDistr5::new(distr.clone());
-    let sum = ks.iter().map(|&k| distr1.ln_pmf(k)).sum::<f64>();
-    let duration = start.elapsed();
-    println!("CachedDistr5: {:?}   (sum {:>})", duration, sum);
-}
-
-pub fn test() {
-    let m = 30.0;
-    let v = 35.0;
-    println!("Start");
-    let distr = NBinom::estimate(m, v);
-    let n = distr.n();
-    let p = distr.p();
-    // let s = <NBinom as DiscreteCDF<u64, f64>>::inverse_cdf(&distr, 0.999) as usize;
-    let s = 100;
-    println!("m = {:?},  v = {:?},   n = {:?},  p = {:?},  s = {}", m, v, n, p, s);
-
-    let mut rng = thread_rng();
-    // let vals: Vec<u64> = NegativeBinomial::new(n, p).unwrap().sample_iter(&mut rng).take(100000).collect();
-    let vals: Vec<u64> = (0..1000000).map(|_| rng.gen_range(0..100)).collect();
-    println!("Benchmarking on {} values between {} and {}",
-        vals.len(), vals.iter().min().unwrap(), vals.iter().max().unwrap());
-    bench0(&distr, s, &vals);
-    bench1(&distr, s, &vals);
-    bench2(&distr, s, &vals);
-    bench3(&distr, s, &vals);
-    bench4(&distr, s, &vals);
-    bench5(&distr, s, &vals);
 }
