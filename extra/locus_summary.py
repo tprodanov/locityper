90cfad66796e1e4f12335f65bf54cca7c7f1298c
Timofey Prodanov <timofey.prodanov@gmail.com>
2023-09-06 15:52:38 +0200

    Update suppl script

--- extra/locus_summary.py
+++ extra/locus_summary.py
@@ -6,6 +6,7 @@ import gzip
 import numpy as np
 import tqdm
 import sys
+import lz4.frame
 
 from common import open_stream
 
@@ -29,7 +30,7 @@ def process_locus(dir, out):
         start = int(start)
         end = int(end)
 
-    with gzip.open(os.path.join(dir, 'kmers.gz'), 'rt') as inp:
+    with lz4.frame.open(os.path.join(dir, 'kmers.lz4'), 'rt') as inp:
         next(inp)
         kmers = np.fromiter(map(int, next(inp).strip().split()), dtype=np.uint16)
 
@@ -41,19 +42,19 @@ def process_locus(dir, out):
     assert n * (n - 1) / 2 == len(divergencies)
     aver_dist = np.mean(divergencies)
     out.write(f'{name}\t{chrom}:{start+1}-{end}\t{end-start}\t{n}\t{m}\t{aver_dist:.7f}')
-    for i in range(1, N_CDFS + 1):
+    for i in range(1, KMER_COLS + 1):
         out.write('\t{:.4f}'.format(np.mean(kmers <= i)))
     out.write('\n')
     out.flush()
 
 
-N_CDFS = 5
+KMER_COLS = 5
 
 
 def main():
     parser = argparse.ArgumentParser(description='Calculates average distance between gene alleles')
-    parser.add_argument('input', metavar='DIR',
-        help='Input directory, with multiple loci.')
+    parser.add_argument('db', metavar='DIR',
+        help='Database directory.')
     parser.add_argument('-L', '--loci', metavar='FILE',
         help='Only analyze loci with names from this list.')
     parser.add_argument('-o', '--output', metavar='FILE', required=False,
@@ -62,18 +63,19 @@ def main():
 
     out = open_stream(args.output, 'w')
     out.write('locus\tregion\tlength\thaplotypes\tfilt_haplotypes\taver_dist')
-    for i in range(1, N_CDFS + 1):
-        out.write(f'\tcdf{i}')
+    for i in range(1, KMER_COLS + 1):
+        out.write(f'\tkmers{i}')
     out.write('\n')
 
+    loci_dir = os.path.join(args.db, 'loci')
     if args.loci is not None:
         with open(args.loci) as inp:
             loci = list(filter(bool, map(str.strip, inp)))
     else:
-        loci = os.listdir(args.input)
+        loci = os.listdir(loci_dir)
 
     for locus in tqdm.tqdm(loci):
-        dir = os.path.join(args.input, locus)
+        dir = os.path.join(loci_dir, locus)
         if os.path.isfile(os.path.join(dir, 'success')):
             process_locus(dir, out)
 
