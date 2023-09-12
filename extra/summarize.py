24fa12014c3bdefa6d4e41f78707466dc484611d
Timofey Prodanov <timofey.prodanov@gmail.com>
2023-09-12 09:02:55 +0200

    Improve summary script

--- extra/summarize.py
+++ extra/summarize.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 import argparse
 from multiprocessing import Pool
 import itertools
@@ -9,25 +10,50 @@ import pandas as pd
 import functools
 from scipy.stats import pearsonr, spearmanr
 from tqdm import tqdm
+import re
+import gzip
+import json
 
 from common import open_stream
 
 
-def load_tags(path):
-    tags = []
-    tag_vals = []
-    if path is not None:
-        with open_stream(path) as f:
-            for line in f:
-                line_split = line.strip().split()
-                tags.append(line_split[0].rstrip(':'))
-                tag_vals.append(tuple(line_split[1:]))
-                assert tags[-1] and tag_vals[-1]
-    tag_tuples = list(itertools.product(*tag_vals))
-    return tuple(tags), tag_tuples
+def _recursive_find_tuples(path, matches, all_tuples, curr_tuple=(), depth=0, shift=0):
+    if depth == len(matches):
+        all_tuples.append(curr_tuple)
+        return
+    m = matches[depth]
+    dir = path[ : m.start() + shift]
+    suffix = path[m.end() : ]
+
+    subdirs = []
+    with os.scandir(dir) as it:
+        for entry in it:
+            if not entry.name.startswith('.') and entry.is_dir():
+                subdirs.append(entry)
+
+    for entry in subdirs:
+        _recursive_find_tuples(entry.path + suffix, matches, all_tuples,
+            curr_tuple + (entry.name,), depth + 1, len(entry.path) - m.end())
 
 
-def process(liks, dist):
+def load_tags(path1, path2):
+    matches = list(re.finditer(r'\{([a-zA-Z0-9_]+)\}', path1))
+    tags = [m.group(1) for m in matches]
+    all_tuples = []
+    _recursive_find_tuples(path1, matches, all_tuples)
+
+    filt_tuples = []
+    for tup in all_tuples:
+        d = dict(zip(tags, tup))
+        if os.path.exists(path2.format(**d)):
+            filt_tuples.append(tup)
+
+    sys.stderr.write('Found {} tag combinations. Discarded {} of them\n'.format(
+        len(all_tuples), len(all_tuples) - len(filt_tuples)))
+    return tags, filt_tuples
+
+
+def process(res, liks, dist):
     liks = liks.groupby('genotype').last().reset_index()
     merged = liks.merge(dist, on='genotype').reset_index(drop=True)
     total_gts = merged.shape[0]
@@ -35,56 +61,66 @@ def process(liks, dist):
     merged = merged.sort_values(['dist', 'lik'], ascending=[True, False]).reset_index(drop=True)
     top_lik_ix = np.argmax(merged.lik)
 
-    s = '{}\t{}\t{:.10}\t{:.10}'.format(total_gts, merged.shape[0],
+    s = '{}\t{}\t{:.10f}\t{:.10f}'.format(total_gts, merged.shape[0],
         pearsonr(merged.lik, merged.dist).statistic,
         spearmanr(merged.lik, merged.dist).statistic)
     s += '\t{}\t{}\t{}\t{}'.format(merged.genotype[0], merged.dist[0], merged.lik[0],
         (merged.lik > merged.lik[0]).sum() + 1)
     i = np.argmax(merged.lik)
-    s += '\t{}\t{}\t{}\t{}\n'.format(merged.genotype[i], merged.dist[i], merged.lik[i], i + 1)
+    s += '\t{}\t{}\t{}\t{}'.format(merged.genotype[i], merged.dist[i], merged.lik[i], i + 1)
+
+    merged.set_index('genotype', inplace=True)
+    weighted_dist = 0.0
+    weight_sum = 0.0
+    for option in res['options']:
+        w = option['prob']
+        weighted_dist += merged.loc[option['genotype']].dist * w
+        weight_sum += w
+    weighted_dist /= weight_sum
+
+    s += '\t{:.5f}\t{:.10f}\n'.format(res['quality'], weighted_dist)
     return s
 
 
-def load_and_process(tup, tags, lik_fmt, dist_fmt):
+def load_and_process(tup, tags, input_fmt, dist_fmt):
     d = dict(zip(tags, tup))
-    liks = pd.read_csv(lik_fmt.format(**d), sep='\t', comment='#')
+    input_dir = input_fmt.format(**d)
+    with gzip.open(os.path.join(input_dir, 'res.json.gz'), 'rt') as f:
+        res = json.load(f)
+    liks = pd.read_csv(os.path.join(input_dir, 'lik.csv.gz'), sep='\t', comment='#')
     dist = pd.read_csv(dist_fmt.format(**d), sep='\t', comment='#')
-    s = process(liks, dist)
+    s = process(res, liks, dist)
     return '\t'.join(tup + (s,))
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='Summarize genotyping result.',
-        usage='%(prog)s -i dir -d dist.csv')
-    parser.add_argument('-g', '--genotyping', metavar='STR',
+        usage='%(prog)s -i path -d path -o out.csv [-@ threads]')
+    parser.add_argument('-i', '--input', metavar='STR', required=True,
         help='Path to genotyping results. Parts like `{tag}` are replaced, tags are supplied separately. '
             'Input directories must contain `lik.csv.gz` and `res.json.gz` files.')
-    parser.add_argument('-d', '--distances', metavar='FILE',
+    parser.add_argument('-d', '--distances', metavar='STR',  required=True,
         help='Path to distances. Parts like `{tag}` are replaced.')
-    parser.add_argument('-t', '--tags', metavar='FILE',
-        help='Optional: file, where each line starts with `tag:`, and contains all possible values through whitespace.')
-    parser.add_argument('-o', '--output', metavar='FILE',
+    parser.add_argument('-o', '--output', metavar='FILE',  required=True,
         help='Output CSV file.')
     parser.add_argument('-@', '--threads', metavar='INT', type=int, default=8,
         help='Number of threads [%(default)s].')
     args = parser.parse_args()
 
-    tags, tag_tuples = load_tags(args.tags)
+    tags, tag_tuples = load_tags(args.input, args.distances)
 
     out = open_stream(args.output, 'w')
     out.write('# {}\n'.format(' '.join(sys.argv)))
     for tag in tags:
         out.write(tag + '\t')
     out.write('total_gts\test_gts\tpearsonr\tspearmanr\tclosest_gt\tclosest_gt_dist\tclosest_gt_lik\tclosest_lik_rank'
-        '\tml_gt\tml_gt_dist\tml_gt_lik\tml_dist_rank\n')
+        '\tml_gt\tml_gt_dist\tml_gt_lik\tml_dist_rank\tgt_qual\tweighted_dist\n')
 
-    lik_fmt = args.genotyping + '/lik.csv.gz'
-    dist_fmt = args.distances
 
     n = len(tag_tuples)
     threads = min(n, args.threads)
-    f = functools.partial(load_and_process, tags=tags, lik_fmt=lik_fmt, dist_fmt=dist_fmt)
+    f = functools.partial(load_and_process, tags=tags, input_fmt=args.input, dist_fmt=args.distances)
 
     if threads > 1:
         with Pool(threads) as pool:
