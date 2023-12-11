854be1e39917d59c3ac47c371e83363f0fb18164
Timofey Prodanov <timofey.prodanov@gmail.com>
2023-12-11 12:25:58 +0100

    Make `into_vcf` multi-threaded

--- extra/into_vcf.py
+++ extra/into_vcf.py
@@ -9,6 +9,8 @@ import gzip
 import os
 import collections
 import glob
+import multiprocessing
+import functools
 
 import common
 
@@ -23,7 +25,7 @@ def load_input(args):
     if args.input is not None:
         in_dirs = args.input
         if len(in_dirs) == 1 and '*' in in_dirs[0]:
-            paths = glob.glob(in_dirs[0])
+            in_dirs = glob.glob(in_dirs[0])
         for path in in_dirs:
             components = path.split('/')
             ixs = [i for i, comp in enumerate(components) if comp == '.']
@@ -57,26 +59,30 @@ def load_input(args):
     return paths
 
 
-def load_database(path, contig_lengths):
+def load_database(path, subset_loci):
     loci_dir = os.path.join(path, 'loci')
     if not os.path.isdir(loci_dir):
         error(f'Directory `{path}` does not contain "loci" subdirectory')
     loci = []
+    if subset_loci is not None:
+        subset_loci = set(subset_loci)
 
     for entry in os.scandir(os.path.join(path, 'loci')):
         if not entry.is_dir():
             continue
         locus = entry.name
-        locus_dir = entry.path
+        if subset_loci is not None and locus not in subset_loci:
+            continue
 
+        locus_dir = entry.path
         with open(os.path.join(locus_dir, 'ref.bed')) as f:
             chrom, start, end, name = next(f).strip().split()
         if name != locus:
             error(f'Locus directory `{locus_dir}` contains locus `{name}` (expected `{locus}`)')
         start = int(start)
         end = int(end)
-        if end > contig_lengths.get(chrom, end):
-            error(f'Variant file does is missing chromosome {chrom}, or it is too short')
+        # if chrom not in contigs or end > contigs[chrom].length:
+        #     error(f'Chromosome {chrom} is missing or it is too short in the input VCF file')
         loci.append((locus, chrom, start, end))
     sys.stderr.write(f'Loaded {len(loci)} loci\n')
     return loci
@@ -101,6 +107,10 @@ def load_predictions(input_paths, locus):
         with gzip.open(os.path.join(dirname, 'loci', locus, 'res.json.gz'), 'rt') as inp:
             res = json.load(inp)
             pred = {}
+            if 'genotype' not in res:
+                pred = None
+                continue
+
             pred['GT'] = res['genotype']
             gq = np.floor(float(res['quality']) * 10) / 10
             if 'warnings' in res:
@@ -131,12 +141,13 @@ def copy_genotype(var, pred_gt, genome_names):
     return out_gt
 
 
-def process_locus(variants, contig_lengths, input_paths, locus, genome_names, output):
+def process_locus(locus, input_paths, genome_names, output):
     locus, chrom, start, end = locus
     predictions = load_predictions(input_paths, locus)
     out_filename = os.path.join(output, f'{locus}.vcf.gz')
-    header = create_vcf_header(chrom, contig_lengths[chrom], input_paths)
 
+    global variants
+    header = create_vcf_header(chrom, variants.header.contigs[chrom].length, input_paths)
     with pysam.VariantFile(out_filename, 'wz', header=header) as out_vcf:
         for var in variants.fetch(chrom, start, end):
             newvar = out_vcf.new_record()
@@ -152,6 +163,12 @@ def process_locus(variants, contig_lengths, input_paths, locus, genome_names, ou
                     if key in pred:
                         fmt[key] = pred[key]
             out_vcf.write(newvar)
+    return locus
+
+
+def create_thread(vcf_filename):
+    global variants
+    variants = pysam.VariantFile(vcf_filename)
 
 
 def main():
@@ -164,15 +181,19 @@ def main():
     in_group.add_argument('-I', '--input-list', metavar='FILE',
         help='File with two columns: path to Locityper analysis and sample name.\n'
             'Mutually exclusive with `-i/--input`.')
+    parser.add_argument('-d', '--database', metavar='FILE', required=True,
+        help='Path to Locityper database.')
     parser.add_argument('-v', '--variants', metavar='FILE', required=True,
         help='Indexed phased variants file.\n'
             'Must have matching sample names with the loci alleles.')
-    parser.add_argument('-d', '--database', metavar='FILE', required=True,
-        help='Path to Locityper database.')
     parser.add_argument('-o', '--output', metavar='DIR', required=True,
         help='Output directory.')
     parser.add_argument('-g', '--genome', metavar='STR',
         help='Reference genome name (if the script cannot guess).')
+    parser.add_argument('-@', '--threads', metavar='INT', type=int, default=8,
+        help='Analyze loci in this many threads [%(default)s].')
+    parser.add_argument('--subset-loci', metavar='STR', nargs='+',
+        help='Limit the analysis to these loci.')
     args = parser.parse_args()
 
     try:
@@ -181,18 +202,30 @@ def main():
         pass
 
     input_paths = load_input(args)
-    variants = pysam.VariantFile(args.variants)
-    contig_lengths = { contig.name: contig.length for contig in variants.header.contigs.itervalues() }
-    loci = load_database(args.database, contig_lengths)
+    loci = load_database(args.database, args.subset_loci)
 
     if args.genome:
         genome_names = { args.genome }
     else:
         genome_names = { 'GRCh38', 'GRCh37', 'hg38', 'hg19', 'CHM13' }
 
-    for i, locus in enumerate(loci, 1):
-        sys.stderr.write('[{:3}/{}] {}\n'.format(i, len(loci), locus[0]))
-        process_locus(variants, contig_lengths, input_paths, locus, genome_names, args.output)
+    total = len(loci)
+    finished = 0
+    def callback(locus):
+        nonlocal finished
+        finished += 1
+        sys.stderr.write(f'Finished [{finished:3}/{total}] {locus}\n')
+
+    with multiprocessing.Pool(args.threads, initializer=create_thread, initargs=(args.variants,)) as pool:
+        # run_locus = functools.partial(process_locus,
+        #     input_paths=input_paths, genome_names=genome_names, output=args.output)
+        # pool.map_async(run_locus, loci, callback=callback, chunksize=1)
+        results = [pool.apply_async(process_locus, (locus, input_paths, genome_names, args.output), callback=callback)
+            for locus in loci]
+        for res in results:
+            res.get()
+        pool.close()
+        pool.join()
 
 
 if __name__ == '__main__':
